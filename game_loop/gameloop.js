30b6d6dbcba63b02bf9ec510c2656109e46f21ae
a-kropotin <a.kropotin@pixonic.com>
2024-06-01 19:02:51 +0300

    chore: extract gameloop into JS file

--- game_loop/gameloop.js
+++ game_loop/gameloop.js
@@ -1,45 +1,15 @@
-const GameStatus = {
-  RUNNING: 'RUNNING',
-  STOPPED: 'STOPPED'
-};
-
-class Ball {
-  constructor(canvas, radius, speed) {
-    this.canvas = canvas;
-    this.context = canvas.getContext('2d');
-    this.radius = radius;
-    this.speed = speed;
-    this.x = radius;
-    this.y = canvas.height / 2;
-    this.direction = 1;
-  }
-
-  update(elapsedTime) {
-    const step = this.speed * elapsedTime / 1000;
-    this.x += step * this.direction;
-    if (this.x + this.radius >= this.canvas.width || this.x - this.radius <= 0) {
-      this.direction *= -1;
-    }
-  }
-
-  render() {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.context.beginPath();
-    this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    this.context.fillStyle = 'black';
-    this.context.fill();
-    this.context.closePath();
-  }
-
-  setSpeed(speed) {
-    this.speed = speed;
-  }
-}
-
 class GameLoop {
+  
+  static GameStatus = {
+    RUNNING: 'RUNNING',
+    STOPPED: 'STOPPED'
+  };
+
+  #targetFPS;
+  #frameDuration;
+
   constructor(maxFPS, ball) {
-    this.logger = console;
-    this.status = GameStatus.STOPPED;
+    this.status = GameLoop.GameStatus.STOPPED;
     this.targetFPS = maxFPS;
     this.frameDuration = 1000 / maxFPS;
     this.ball = ball;
@@ -54,18 +24,18 @@ class GameLoop {
   }
 
   run() {
-    this.status = GameStatus.RUNNING;
+    this.status = GameLoop.GameStatus.RUNNING;
     this.previousTime = Date.now();
     this.gameLoop();
   }
 
   stop() {
-    this.status = GameStatus.STOPPED;
+    this.status = GameLoop.GameStatus.STOPPED;
     cancelAnimationFrame(this.requestId);
   }
 
   isGameRunning() {
-    return this.status === GameStatus.RUNNING;
+    return this.status === GameLoop.GameStatus.RUNNING;
   }
 
   processInput() {}
@@ -105,33 +75,5 @@ class GameLoop {
     }
 
     this.requestId = requestAnimationFrame(this.gameLoop.bind(this));
-  }
-
-  setMaxFPS(maxFPS) {
-    this.targetFPS = maxFPS;
-    this.frameDuration = 1000 / maxFPS;
-  }
-}
-
-let canvas = document.getElementById('canvas');
-let fpsInput = document.getElementById('fpsInput');
-let speedInput = document.getElementById('speedInput');
-let ball = new Ball(canvas, 20, parseInt(speedInput.value, 10));
-let gameLoop = new GameLoop(parseInt(fpsInput.value, 10), ball);
-
-function startGame() {
-  if (gameLoop.isGameRunning()) {
-    gameLoop.stop();
-  }
-
-  ball.setSpeed(parseInt(speedInput.value, 10));
-  gameLoop.setMaxFPS(parseInt(fpsInput.value, 10));
-  
-  gameLoop.run();
-}
-
-function stopGame() {
-  if (gameLoop) {
-    gameLoop.stop();
   }
 }
---
+++ game_loop/main.js
@@ -0,0 +1,55 @@
+class Ball {
+  constructor(canvas, radius, speed) {
+    this.canvas = canvas;
+    this.context = canvas.getContext('2d');
+    this.radius = radius;
+    this.speed = speed;
+    this.x = radius;
+    this.y = canvas.height / 2;
+    this.direction = 1;
+  }
+
+  update(elapsedTime) {
+    const step = this.speed * elapsedTime / 1000;
+    this.x += step * this.direction;
+    if (this.x + this.radius >= this.canvas.width || this.x - this.radius <= 0) {
+      this.direction *= -1;
+    }
+  }
+
+  render() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.context.beginPath();
+    this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    this.context.fillStyle = 'black';
+    this.context.fill();
+    this.context.closePath();
+  }
+
+  setSpeed(speed) {
+    this.speed = speed;
+  }
+}
+
+let canvas = document.getElementById('canvas');
+let fpsInput = document.getElementById('fpsInput');
+let speedInput = document.getElementById('speedInput');
+let ball = new Ball(canvas, 20, parseInt(speedInput.value, 10));
+let gameLoop = new GameLoop(parseInt(fpsInput.value, 10), ball);
+
+function startGame() {
+  if (gameLoop.isGameRunning()) {
+    gameLoop.stop();
+  }
+
+  ball.setSpeed(parseInt(speedInput.value, 10));
+  gameLoop = new GameLoop(parseInt(fpsInput.value, 10), ball);
+  
+  gameLoop.run();
+}
+
+function stopGame() {
+  if (gameLoop) {
+    gameLoop.stop();
+  }
+}
