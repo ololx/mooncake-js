5fe0c9fcaf410952b7ff6f9e96b892eeb4aa5a21
a-kropotin <a.kropotin@pixonic.com>
2024-11-09 21:12:57 +0200

    chore: implement resource manager

--- assets.js
+++ assets.js
@@ -78,3 +78,112 @@ export class Assets {
         });
     }
 }
+
+export class ResourceManager {
+    constructor(gl) {
+        this.gl = gl;
+        this.resources = new Map();
+    }
+
+    importResource(filePath, type, importSettings = { compressed: false, format: 'default' }) {
+        const guid = this.generateGUID(filePath);
+
+        if (this.resources.has(guid)) {
+            return this.resources.get(guid);
+        }
+
+        const resourceMeta = {
+            guid,
+            filePath,
+            type,
+            importSettings,
+            data: null,
+            usageCount: 0
+        };
+
+        this.resources.set(guid, resourceMeta);
+        return resourceMeta;
+    }
+
+    async loadResource(filePath) {
+        const guid = this.generateGUID(filePath);
+
+        if (!this.resources.has(guid)) {
+            throw new Error(`Resource ${filePath} not imported. Please import it first.`);
+        }
+
+        const resource = this.resources.get(guid);
+
+        if (!resource.data) {
+            resource.data = await this._loadResourceData(resource);
+        }
+
+        resource.usageCount += 1;
+        return resource.data;
+    }
+
+    async _loadResourceData(resource) {
+        if (resource.type === 'image') {
+            return await this.loadImage(resource.filePath, resource.importSettings);
+        } else if (resource.type === 'audio') {
+            return await this.loadAudio(resource.filePath);
+        } else {
+            throw new Error(`Unknown resource type: ${resource.type}`);
+        }
+    }
+
+    async loadImage(filePath, importSettings) {
+        const image = await new Promise((resolve, reject) => {
+            const img = new Image();
+            img.src = filePath;
+            img.onload = () => resolve(img);
+            img.onerror = reject;
+        });
+
+        if (importSettings.compressed) {
+        }
+
+        return this.createTextureFromImage(image);
+    }
+
+    async loadAudio(filePath) {
+        const audio = new Audio(filePath);
+        await audio.load();
+        return audio;
+    }
+
+    createTextureFromImage(image) {
+        const gl = this.gl;
+        const texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+        gl.generateMipmap(gl.TEXTURE_2D);
+        return texture;
+    }
+
+    releaseResource(filePath) {
+        const guid = this.generateGUID(filePath);
+        if (this.resources.has(guid)) {
+            const resource = this.resources.get(guid);
+            resource.usageCount -= 1;
+
+            if (resource.usageCount <= 0) {
+                this.unloadResource(guid);
+            }
+        }
+    }
+
+    unloadResource(guid) {
+        const resource = this.resources.get(guid);
+        if (resource && resource.data) {
+            if (resource.type === 'image' && resource.data instanceof WebGLTexture) {
+                this.gl.deleteTexture(resource.data);
+            }
+            resource.data = null;
+        }
+    }
+
+    generateGUID(filePath) {
+        return btoa(filePath);
+    }
+}
